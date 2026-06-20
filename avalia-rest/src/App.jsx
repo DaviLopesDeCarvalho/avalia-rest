8c3a646db296944a3d0c19c2b51ef5ea44803099
DaviLopesDeCarvalho <davilopes1014@gmail.com>
2026-06-20 14:21:00 -0300

    feat: cria arquivos de estilo base e componente visual de card

--- avalia-rest/src/App.jsx
+++ avalia-rest/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { RestaurantProvider } from './context/RestaurantContext';
 import Navbar from './components/Navbar';
+import './styles/global.css'
 
 export default function App() {
   return (
